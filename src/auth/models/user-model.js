b898eebca2cb62c91255fdb60bc522a5feb23e9f
Ryan Gallaway <rkgallaway@msn.com>
2022-09-27 13:05:20 -0700

    bearer auth complete

--- src/auth/models/user-model.js
+++ src/auth/models/user-model.js
@@ -3,6 +3,9 @@
 // const bcrypt = require('bcrypt');
 // const base64 = require('base-64');
 const { Sequelize, DataTypes } = require('sequelize');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const SECRET = process.env.API_SECRET || 'ThisIsMySecret';
 
 // we'll use this for inclass-demo.  one big monolithic file  
 const DATABASE_URL = process.env.NODE_ENV === 'test'
@@ -34,12 +37,51 @@ const UsersModel = sequelizeDatabase.define('Users', {
     type: DataTypes.STRING,
     allowNull: false,
   },
+  token: {
+    type: DataTypes.VIRTUAL,
+    get(){ // a method that "gets" called on "read"
+      return jwt.sign({username: this.username}, SECRET, {expiresIn: 86400000});
+    },
+    set(){ // a method that runs when set with "="
+      return jwt.sign({username: this.username}, SECRET, {expiresIn: 86400000});
+    },
+  },
 });
 
 // Attach beforeCreate Hook to the UserModel.  
-UsersModel.beforeCreate((user) => {
-  console.log('our user', user);
-});
+// whole point:  know that exists, and play with a hook. we've done that
+// UsersModel.beforeCreate((user) => {
+//   console.log('our user', user);
+// });
+
+// for your own edification.... do we need this maybe / maybe not
+UsersModel.authenticateBasic = async (username, password)=> {
+  try {
+    const user = await this.findOne({where: { username}});
+    const valid = await bcrypt.compare(password, user.password);
+    if (valid){
+      return  user;
+    }
+  } catch (e) {
+    console.error(e);
+    return e;
+  }
+};
+
+UsersModel.authenticateBearer = async (token) => {
+  try {
+    let payload = jwt.verify(token, SECRET);
+    console.log('from authenticateBearer', payload);
+
+    const user = await this.findOne({where: {username: payload.username}});
+    if(user){
+      return user;
+    }
+  }catch (e){
+    console.error(e);
+    return e;
+  }
+};
 
 module.exports = {
   UsersModel,
--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@
 const express = require('express');
 const authRouter = require('./auth/router');
 const basicAuth = require('./auth/middleware/basic');
+const bearerAuth= require('./auth/middleware/bearer');
+const { UsersModel } = require('./auth/models/user-model');
 const app = express();
 const PORT = process.env.PORT || 3002;
 
@@ -23,6 +25,18 @@ app.use(authRouter);
 app.get('/hello', basicAuth, (req, res, next) => {
   let { name } = req.query;
   res.status(200).send(`Greetings ${name}! this route is now secured by Basic AUth!!!`);
+});
+
+app.get('/users', bearerAuth, async (req, res, next) => {
+  console.log('from the users get route', req.user);
+  let user = await UsersModel.findAll();
+  // use ris an unresolved promise that contains
+  let payload = {
+    results: user,
+  };
+
+  res.status(200).send(payload);
+
 });
 
 // export app for testing, start ability to run app, and our db with ORM
